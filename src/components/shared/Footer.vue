9918b551ca090d2bfca230d8fb0c4730ca7969b6
Andrea De Martino <ademartino1992@gmail.com>
2020-09-25 12:43:06 +0200

    Footer Created without animations

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,9 +1,90 @@
 <template>
-  <h2>SONO L'HEADER</h2>
+  <footer class="footer">
+    <div class="footer__logo">
+      <img src="../../assets/images/logo-footer.png" alt="" />
+    </div>
+    <div class="footer__sections">
+      <p>we are</p>
+      <p>we do</p>
+      <p>we careers</p>
+      <p>we contact us</p>
+    </div>
+    <div class="footer__icons">
+      <i class="fab fa-twitter"></i>
+      <i class="fab fa-facebook-f"></i>
+      <i class="fab fa-instagram"></i>
+    </div>
+  </footer>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.footer {
+  min-height: 350px;
+  width: 100%;
+  background-color: $dark;
+  @include u-flex(column, center, center);
+  &__logo {
+    img {
+      max-width: 250px;
+    }
+  }
+  &__sections {
+    max-width: 450px;
+    @include u-flex();
+    margin: 60px 0;
+    p {
+      color: $light;
+      padding-right: 100px;
+      white-space: nowrap;
+      &:last-child {
+        padding-right: 0;
+      }
+    }
+  }
+  &__icons {
+    i {
+      color: $light;
+      padding: 10px;
+      font-size: 20px;
+    }
+  }
+}
+
+//Responsive
+
+@include mobile {
+  footer {
+    .footer__logo {
+      margin-top: 50px;
+    }
+    .footer__sections {
+      margin: 40px 0;
+      @include u-flex(column, center, center);
+      p {
+        padding-right: 0;
+        padding: 10px 0;
+      }
+    }
+  }
+}
+
+@include small-mobile {
+  footer {
+    .footer__logo {
+      margin-top: 50px;
+    }
+    .footer__sections {
+      margin: 40px 0;
+      @include u-flex(column, center, center);
+      p {
+        padding-right: 0;
+        padding: 10px 0;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,29 @@
 <template>
-<div class="home">
-  <app-header></app-header>
-  <app-main></app-main>
-</div>
-
-  
+  <div class="home">
+    <app-header></app-header>
+    <app-main></app-main>
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
 // Components
 import Header from "../components/shared/Header.vue";
 import Main from "../components/shared/Main.vue";
-
+import Footer from "../components/shared/Footer.vue";
 
 export default {
   components: {
     appHeader: Header,
     appMain: Main,
+    appFooter: Footer,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.home{
+.home {
   position: relative;
-  width: 100%;
-}
-.section{
-  background: #ccc;
-  height: 100vh;
   width: 100%;
 }
 </style>
